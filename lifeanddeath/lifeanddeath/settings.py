b15b41cbe38e49d598cd1413633ccf8ce374b4e6
Christoph Royer <mail@christophroyer.com>
2022-06-14 21:44:09 +0200

    Moved Debug to local_settings

--- lifeanddeath/lifeanddeath/settings.py
+++ lifeanddeath/lifeanddeath/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'dewwcbdgvnnaizgsauszl.ml']
 
